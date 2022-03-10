a680112d30a59aecfdc8adad262a0458c4ac30f5
SK <senthilptj@gmail.com>
2022-03-10 23:27:15 +0530

    UC4 Deleting Contacts

--- src/test/java/com/bridgelabz/day22/AddressBookMainTest.java
+++ src/test/java/com/bridgelabz/day22/AddressBookMainTest.java
@@ -3,11 +3,12 @@ package com.bridgelabz.day22;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AddressBookMainTest {
+class AddressBookMainTest
+{
     @Test
     public void shouldAnswerWithTrue()
     {
-        System.out.println("Adding new contact to address book ");
+        System.out.println("Delete a person using person's name");
         assertTrue( true );
     }
 }
