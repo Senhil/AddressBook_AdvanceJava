4ebc258d9986011d15b4a50a1021c18a9e74ec75
SK <senthilptj@gmail.com>
2022-03-10 23:12:00 +0530

    Adding Multiple Address

---
+++ src/test/java/com/bridgelabz/day22/AddressBookMainTest.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.day22;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookMainTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("adding multiple Address Book to the System");
+        assertTrue( true );
+    }
+}
